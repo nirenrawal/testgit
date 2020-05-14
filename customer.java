e53004ebba9e3cc606b71de51c5aa3d2654f043a
niren <nirenrawal@hotmail.com>
2020-05-14 18:02:42 +0200

    new printline added

--- customer.java
+++ customer.java
@@ -1,5 +1,6 @@
 public class Customer{
    public static void main (String [] args){
       System.out.println("The customer went to buy covid19 vaccine");
+      System.out.println("What is your name?");
    }
 }
